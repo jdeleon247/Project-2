6832d7192992c03c5675e1e5173524bb4fe061f6
Jonathan DeLeon <jonathandeleon@gmail.com>
2020-10-27 00:05:13 -0400

    added check for root and check for group functions

--- mail.sh
+++ mail.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
+
+group="CSI230"
+checkRoot()
+{
+   if [[ $EUID -ne 0 ]]; 
+      then
+      echo "This script must be run as root" 
+      exit 1
+   fi
+}
+
+checkGroup()
+{
+   if grep -q $group /etc/group   
+   then
+      echo "group exists"
+   else
+      echo "creating group "$group 
+      groupadd $group
+   fi
+}
+#Main
+
+checkRoot
+checkGroup
