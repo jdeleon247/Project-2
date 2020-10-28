a8bc9890241296249766c8e8b61775e8dc2c42a3
Jonathan DeLeon <jonathandeleon@gmail.com>
2020-10-28 16:39:18 -0400

    modified testemail script

--- testemail.sh
+++ testemail.sh
@@ -1,1 +1,4 @@
-echo "email test body" | s-nail -v -s "email test subject" "tillyboyd@protonmail.com" -r "jonathan.deleon@mymail.champlain.edu"
+#echo "email test body" | s-nail -v -s "email test subject" "tillyboyd@protonmail.com" -r "jonathan.deleon@mymail.champlain.edu"
+
+
+echo -e "test email script body" | s-nail -S smtp-use-starttls -S ssl-verify=ignore -S smtp-auth=login -S smtp="gmail.com:587" -S smtp-auth-user="jonathan.deleon@mymail.champlain.edu" -S smtp-auth-password="sjsuvtgzseisaytg" -r "jonathan.deleon@mymail.champlain.edu" -s "test email script header" -. "tillyboyd@protonmail.com"
